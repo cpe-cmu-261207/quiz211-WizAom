73e8b864988ab4c408b17a9bb99524ff87203786
WizAom <40514176+WizAom@users.noreply.github.com>
2021-02-09 13:24:52 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,51 +1,39 @@
+import { useState } from "react";
+import { RunPerson } from "./components/RunPerson";
+
 function App() {
-  const persons = (props) => {
-    return (
-      <label for = "name"> 
-        {props.name}: 
-      </label>
-      //<label for = "gender"> {props.gender}: </label>
-      //<input type= "text" name={props.name} />
-    )
-  }
+  const persons = [
+    {
+      name: "Bob",
+      gender: "male",
+      age: "50"
+    },
+    {
+      name: "Alice",
+      gender: "male",
+      age: "20"
+    },
+    {
+      name: "Sarinya Pamontree",
+      gender: "female",
+      age: "20"
+    }
+  ];
+  const [count, setCount] = useState(620610810);
   return (
     <div class="ml-2">
       <h3 class="title is-3">Person List</h3>
-
       {/* Convert me to a component! */}
-      <table class="table is-bordered mb-3">
-        <tbody>
-          <tr>
-            <th>Name</th>
-            <th>Gender</th>
-            <th>Age</th>
-          </tr>
-          <tr>
-            <person label ="name" name="Bob"> 
-            <td>Bob</td>
-            <td>male</td>
-            <td>50</td>
-          </tr>
-          <tr>
-            <th>Name</th>
-            <th>Gender</th>
-            <th>Age</th>
-          </tr>
-          <tr>
-            <person label ="name" name="Bob"> 
-            <td>Alice</td>
-            <td>male</td>
-            <td>20</td>
-          </tr>
-        </tbody>
-      </table>
-
+      {persons.map((i) => {
+        return <RunPerson name={i.name} gender={i.gender} age={i.age} />;
+      })}
       {/* Code me please! */}
       <h3 class="title is-3">ID Counter</h3>
       <p>YOUR ID HERE</p>
-      <button>-</button>
-      <button>reset</button>
-      <button>+</button>
+      <p>{count}</p>
+      <button onClick={() => setCount(count - 1)}>-</button>
+      <button onClick={() => setCount(620610810)}>reset</button>
+      <button onClick={() => setCount(count + 1)}>+</button>
     </div>
   );
 }
